26ef1ea5014fa8712c67f95206acb74e1775b72a
abhishekkhedekar <khedekarabhishek@gmail.com>
2022-01-15 14:17:32 +0530

    Modified structure

--- Package.swift
+++ Package.swift
@@ -23,8 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ComponentsPackage",
-            dependencies: [],
-            path: "Sources/ComponentsPackage"
+            dependencies: []
         ),
         .testTarget(
             name: "ComponentsPackageTests",
