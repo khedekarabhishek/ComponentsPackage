3b2719861f6664c5a635275444cf99b9ef5ee14a
abhishekkhedekar <khedekarabhishek@gmail.com>
2022-01-15 14:12:10 +0530

    Path set

--- ComponentsPackage/Package.swift
+++ ComponentsPackage/Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ComponentsPackage",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/ComponentsPackage"
+        ),
         .testTarget(
             name: "ComponentsPackageTests",
             dependencies: ["ComponentsPackage"]),
